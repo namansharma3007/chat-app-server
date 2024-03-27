63066f47071a9ad87ecf41345f76cca5ba95d90c
Naman Sharma <namansharma3007c@gmail.com>
2024-03-27 23:07:20 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { app, server } from './socket/socket.js';
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
