47badc99b5326abfb029df8ae1401bc61b717483
Naman Sharma <namansharma3007c@gmail.com>
2024-03-28 00:00:10 +0530

    changes

--- utils/generateTokenandSetCookie.js
+++ utils/generateTokenandSetCookie.js
@@ -4,7 +4,6 @@ const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	});
-	console.log(token);
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
