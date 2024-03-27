f4996313e17a70815b6f7f2a9bbb04b00b2c7237
Naman Sharma <namansharma3007c@gmail.com>
2024-03-27 21:35:33 +0530

    added socket

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -26,9 +27,14 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-
         // this will run in parallel
         await Promise.all([await conversation.save(),await newMessage.save()]);
+
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            // io.to(<socket_id>).emit() this message is used to send events to specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage);
     } catch (error) {
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { app, server } from './socket/socket.js';
 
 import connectToMongodb from './db/connectToMongodb.js';
 import authRoutes from './routes/auth.routes.js'
 import messagesRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -19,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     connectToMongodb();
     console.log(`Server is runnning on port ${PORT}`);
 })
